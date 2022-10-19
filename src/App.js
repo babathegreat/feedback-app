1c5d23919e2cd6eadb4bfd4bb69499e20fefc862
babathegreat <98428616+babathegreat@users.noreply.github.com>
2022-10-19 15:51:14 -0400

    Put proptypes into header.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import Header from "./components/Header"
 
+
 function App () {
     return(
         <>
-            <Header text='Hello World Destructured'/>
+            {/* JSX comment needs curly brackets - Can set the prop text for this value 
+            <Header text='Hello World Destructured'/> */}
+
+            {/* Added color props to the header below */}
+            <Header bgColor='red' textColor='blue' />
             <div className ='container'>
                 <h1>My App</h1>
             </div>
         </>
     )
 }
+
 
 export default App
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,25 @@
 //import React from 'react'
+import PropTypes from 'prop-types'
 
-function Header({text}) {
+// added props to pass in from the App.js file - bgColor & textColor using destructuring
+// doesn't change until use them inside header...
+function Header({text, bgColor, textColor}) {
+
+    //can use a constant to hold the styles. Otherwise can stick inline but need to use double curly brackets
+    // style={{backgroundColor: 'blue',color: 'red'}}
+
+    const headerStyles = {
+        //Now using the passed in props to do the colors...
+        backgroundColor: bgColor,
+        color: textColor
+
+        //Previous hard coded the colors...
+        //backgroundColor: 'blue',
+        //color: 'red'
+    }
+
   return (
-    <header>
+    <header style={headerStyles}>
         <div className='container'>
             <h2>{text}</h2>
         </div>
@@ -10,6 +27,17 @@ function Header({text}) {
     </header>
    
   )
+}
+
+// This is outside of jsx - regular javascript - sets the default props for header on the 
+// App.js file text to Feedback UI
+
+Header.defaultProps = {
+    text: 'Feedback UI'
+}
+
+Header.propTypes = {
+    text: PropTypes.string
 }
 
 export default Header
