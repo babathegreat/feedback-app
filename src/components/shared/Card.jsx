ff67dfc696a288fef031c046368b56189581e591
babathegreat <98428616+babathegreat@users.noreply.github.com>
2022-10-25 11:13:22 -0400

    Updated proptypes

--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -11,10 +11,30 @@
 
 import React from 'react'
 
+import PropTypes from 'prop-types'
+
 function Card({children, reverse}) {
   return (
-    <div className={`card ${reverse && 'reverse'}`}>{children}</div>
+    //conditional class
+   // <div className={`card ${reverse && 'reverse'}`}>{children}</div>
+
+   //can use conditional style as well to achieve same effect
+    <div className='card' style={{
+        backgroundColor: reverse ? 'rgba(0,0,0,0.4)' : '#fff',
+        color: reverse ? '#fff' : '#000',
+    }}>{children}</div>
   )
+}
+
+//Instead of defining on feedbackitem.jsx can set default value here.
+//reverse : true or reverse : false to show dark or white
+Card.defaultProps = {
+    reverse : true
+}
+
+Card.propTypes = {
+    children: PropTypes.node.isRequired,
+    reverse: PropTypes.bool.isRequired,
 }
 
 export default Card
