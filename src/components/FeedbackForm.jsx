e22f6f8db6b2b31f1f9212ed19ee845bb24d82fe
babathegreat <98428616+babathegreat@users.noreply.github.com>
2022-10-27 23:14:16 -0400

    Added state to input with setText and value

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import Card from './shared/Card'
+import {useState} from 'react'
 
 function FeedbackForm() {
+  const [text, setText] = useState('')
+
+  const handleTextChange = (e) => {
+    setText(e.target.value)
+  }
+
   return (
     <Card>
         <form>
             <h2>How would you rate us?</h2>
 
             <div className='input-group'>
-                <input type='text' placeholder='Write Something'/>
+                <input 
+                    onChange={handleTextChange} 
+                    type='text' 
+                    placeholder='Write Something'
+                    value={text}
+                />
+
                 <button type='submit'>Submit</button>
             </div>
         </form>
