9d65352db738173bb1e015e5595a770abcc1948d
babathegreat <98428616+babathegreat@users.noreply.github.com>
2022-10-28 14:14:57 -0400

    Created button.jsx

---
+++ src/components/shared/Button.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+//prop children because will wrap text inside button.
+
+function Button({ children, version, type, isDisabled }) {
+  return (
+    <div type={type} disabled={isDisabled} className={`btn btn-${version}`}>
+        {children}
+    </div>
+  )
+}
+
+//Set some dedfault props for the button values
+Button.defaultProps = {
+    version: 'primary',
+    type: 'button',
+    isDisabled: false
+
+}
+
+Button.propTypes = {
+    children: PropTypes.node.isRequired,
+    version: PropTypes.string,
+    type: PropTypes.string,
+    isDisabled: PropTypes.bool,
+
+}
+
+export default Button
